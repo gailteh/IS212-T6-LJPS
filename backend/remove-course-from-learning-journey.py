74ebbf2631e5582e3c6e6861f38dd8ef09a8acf7
Tang Xinyi <xinyi.tang.2020@smu.edu.sg>
2022-10-22 23:52:46 +0800

    Connect delete learning journey course to frontend
    NEED DEBUG

--- backend/remove-course-from-learning-journey.py
+++ backend/remove-course-from-learning-journey.py
@@ -1,13 +1,13 @@
-from mimetypes import init
-from flask import Flask
+from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from sqlalchemy import ForeignKey
 
 app = Flask(__name__)
 # initiate database connection
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:root@localhost:8889/learning-journey"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:root@localhost:8889/is212_SPM?collate=utf8_general_ci"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
+                                           'pool_recycle': 280}
 
 db = SQLAlchemy(app)
 
@@ -19,15 +19,15 @@ class Courses(db.Model):
 
     course_code = db.Column(db.String, primary_key=True, nullable=False)
     course_name = db.Column(db.String, nullable=False)
-    course_description = db.Column(db.String, nullable=False)
+    course_desc = db.Column(db.String, nullable=False)
 
-    def __init__(self, course_code, course_name, course_description):
+    def __init__(self, course_code, course_name, course_desc):
         self.course_code = course_code
         self.course_name = course_name
-        self.course_description = course_description
+        self.course_desc = course_desc
 
     def json(self):
-        return {"course_code": self.course_code,"course_name":self.course_name, "course_description": self.course_description}
+        return jsonify({"course_code": self.course_code,"course_name":self.course_name, "course_desc": self.course_desc})
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -61,45 +61,45 @@ class Role(db.Model):
 
 
 class Learning_Journey(db.Model):
-    __tablename__ = 'learning-journey'
+    __tablename__ = 'learning_journey'
 
-    LearningJourney_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    course_code = db.Column(db.String, ForeignKey('course.course_code'), nullable = False)
-    role_code = db.Column(db.Integer, ForeignKey('role.role_code'), nullable = False)
+    lj_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    course_code = db.Column(db.String, db.ForeignKey('courses.course_code'), nullable = False)
+    role_code = db.Column(db.Integer,  db.ForeignKey('roles.role_code'), nullable = False)
 
-    def __init__(self, LearningJourney_id, course_code, role_code):
-        self.LearningJourney_id = LearningJourney_id
+    def __init__(self, lj_id, course_code, role_code):
+        self.lj_id = lj_id
         self.course_code = course_code
         self.role_code = role_code
 
 
     def json(self):
-        return {"LearningJourney_id":self.LearningJourney_id, "course_code":self.course_code, "role_code": self.role_code}
+        return {"lj_id":self.lj_id, "course_code":self.course_code, "role_code": self.role_code}
 
 
-@app.route('/learning_journey/<int:LearningJourney_id>/<string:course_code>', methods=['DELETE'])
+@app.route('/del_ljc/<int:lj_id>/<string:course_code>', methods=['DELETE'])
 # deleting 1 course from learning journey
-def del_course_from_learning_journey(LearningJourney_id, course_code):
+def del_course(lj_id, course_code):
     
-    #  get learning journey row using LearningJourney_id and course_code
-    lj_course = Learning_Journey.query.filter_by(LearningJourney_id=LearningJourney_id, course_code=course_code).first()
+    #  get learning journey row using lj_id and course_code
+    lj_course = Learning_Journey.query.filter_by(lj_id=lj_id, course_code=course_code).first()
 
     # delete
     try:
         db.session.delete(lj_course)
         db.session.commit()
     except:
-        return {
+        return jsonify({
             "code": 500,
             "data": {
                 "lj_course": lj_course
             },
             "message": "An error occurred while deleting the course from learning journey"
-        }
-    return {
+        })
+    return jsonify({
         "code": 200,
         "message": str(lj_course) + " had been successfully deleted."
-    }
+    })
 
 if __name__ == '__main__':
-    app.run(port=4999, debug=True)
+    app.run(port=4888, debug=True)

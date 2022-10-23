75eb8221fef1833eb8ba33891796d349b7fae4d6
Kathy Tong <100739687+kt-x@users.noreply.github.com>
2022-10-23 15:32:39 +0800

    updated Staff frontend and backend remove course from learning journey file

--- backend/remove-course-from-learning-journey.py
+++ backend/remove-course-from-learning-journey.py
@@ -4,10 +4,13 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 # initiate database connection
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:root@localhost:8889/is212_SPM?collate=utf8_general_ci"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:root@localhost:8889/is212_SPM?collate=utf8_general_ci"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
+#                                           'pool_recycle': 280}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root@localhost:3306/is212_spm"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
-                                           'pool_recycle': 280}
 
 db = SQLAlchemy(app)
 

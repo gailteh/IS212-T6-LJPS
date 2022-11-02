efd8e5ce9eb70e0218ec0ba936c8e18ab0dc9a09
Tang Xinyi <xinyi.tang.2020@smu.edu.sg>
2022-11-02 13:39:40 +0800

    LJ Course delete function fixed
    LJ Course delete function fixed;
    old LJ update page deleted;
    changed Learning_Journey_latest.py port to 4848 (got conflict on my side so I changed it, respective links on LJ htmls also changed);
    pass in LJ_ID parameter from ViewLearningJourney.html

--- backend/Learning_Journey_latest.py
+++ backend/Learning_Journey_latest.py
@@ -13,6 +13,7 @@ Assumed JSON:
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root' + \
                                         '@localhost:3306/is212_spm'
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:root@localhost:8889/is212_spm"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
                                            'pool_recycle': 280}
@@ -306,4 +307,4 @@ def add_new_course():
 #     }
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=4848, debug=True)
